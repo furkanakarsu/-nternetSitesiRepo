ec408ab6ce081ac64bbb8042fcf734b973ef64df
Furkan Akarsu <furkannakarsu@gmail.com>
2021-03-22 02:00:40 +0300

    Ana taslak yapıldı. Hakkımda ve Deneyimlerim databaseden listelendi.

---
+++ BlogSitesi/BlogSitesi/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BlogSitesi
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataSet1TableAdapters.HAKKIMDATableAdapter dt = new DataSet1TableAdapters.HAKKIMDATableAdapter();
+            Repeater1.DataSource = dt.HakkımdaListele();
+            Repeater1.DataBind();
+
+            DataSet1TableAdapters.DENEYIMTableAdapter dt1 = new DataSet1TableAdapters.DENEYIMTableAdapter();
+            Repeater2.DataSource = dt1.DeneyimListele();
+            Repeater2.DataBind();
+        }
+    }
+}
